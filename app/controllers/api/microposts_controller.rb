a0f31961f5d003aeb85f96a605e74bffa6ce03bd
morioka1206 <r.morioka@icloud.com>
2021-05-21 18:32:49 +0900

    投稿の更新機能

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -1,5 +1,5 @@
 class Api::MicropostsController < ApplicationController
-  before_action: authenticate, only %i[create]
+  before_action: authenticate, only %i[create update]
 
   def index
     microposts = Micropost.includes(:user)
@@ -12,7 +12,9 @@ class Api::MicropostsController < ApplicationController
   end
 
   def update
-
+    micropost = current_user.microposts.find(params[:id])
+    micropost.update!(micropost_params)
+    render json: micropost, serlializer: MicropostSerializer
   end
 
   def destroy
